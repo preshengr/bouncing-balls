fff10fb4a0ffce526c93ee7befad5b7cc159c8ba
Precious Sunday <preshengr@gmail.com>
2024-12-06 22:35:18 +0100

    added the variables create a rule to get element by Id. created the c .linewidth and gloableAlpha

--- balls.js
+++ balls.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+var canvas = document.getElementById("canvas");
+var c = canvas.getContext("2d");
+var tx = window.innerWidth;
+var ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
